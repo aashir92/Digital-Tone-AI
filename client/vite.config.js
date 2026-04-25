02acdfdf16f8222ee51e99915a78b382545bed87
aashir92 <aashir.hameed69@gmail.com>
2026-04-25 18:16:43 +0500

    Fix iframe relative paths and restore crossorigin attribute

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,6 @@ import { crx } from "@crxjs/vite-plugin";
 import manifest from "./public/manifest.json";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    crx({ manifest }),
-    {
-      name: "remove-crossorigin",
-      transformIndexHtml(html) {
-        return html.replace(/ crossorigin/g, "");
-      }
-    }
-  ]
+  plugins: [react(), crx({ manifest })],
+  base: "",
 });
