6d1de1ca2a9c792778b5ea1989d733b91b258570
aashir92 <aashir.hameed69@gmail.com>
2026-04-25 18:19:34 +0500

    Fix blank screen by removing crossorigin attribute in build

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,15 @@ import { crx } from "@crxjs/vite-plugin";
 import manifest from "./public/manifest.json";
 
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [
+    react(),
+    crx({ manifest }),
+    {
+      name: "remove-crossorigin",
+      transformIndexHtml(html) {
+        return html.replace(/\scrossorigin/g, "");
+      },
+    },
+  ],
   base: "",
 });
